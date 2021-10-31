fa779ad59083f11a6ef18f4a5d593f1edaa25b96
darrenori <87930795+darrenori@users.noreply.github.com>
2021-11-01 04:08:39 +0800

    Delete CTF-Website-Template-2020-master directory

--- CTF-Website-Template-2020-master/js/logout.js
+++
@@ -1,7 +0,0 @@
-function logout(){
-    console.log("LOGGING OUT")
-    sessionStorage.removeItem("token");
-    sessionStorage.removeItem("user_hint");
-    sessionStorage.removeItem("all_puzzle_status")
-    window.location.href='login.html';
-}
